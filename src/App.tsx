03674578e26c225b985eeefc2bbfcf3dd40eee40
Fatima Camero <fcamero12@gmail.com>
2021-11-24 12:19:15 -0800

    add Log In form to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Register from './pages/register';
+import LogIn from './pages/login'
 import './firebasedb'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Cravings</h1>
+      <LogIn />
       <Register />
     </div>
   );
