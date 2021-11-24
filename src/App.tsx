6ff9f63cf488a347b4cff6d7e1444ea86b347c3b
Fatima Camero <fcamero12@gmail.com>
2021-11-24 12:09:52 -0800

    import firebase app and include Register component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Register from './pages/register';
+import './firebasedb'
 
 function App() {
   return (
     <div className="App">
       <h1>Cravings</h1>
+      <Register />
     </div>
   );
 }
