cfec99b7bd4a7caf0f9f5f2c548a9c0227cf99c6
Fatima Camero <fcamero12@gmail.com>
2021-12-06 10:07:32 -0800

    add Routes and nav Links for login and registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+/*
+ * App
+ * 
+ * Resources:
+ * https://reactrouterdotcom.fly.dev/docs/en/v6/getting-started/installation#basic-installation
+*/
 import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home'
 import LogIn from './pages/LogIn';
 import Register from './pages/Register'
@@ -8,9 +15,16 @@ import './firebasedb';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <LogIn />
-      <Register />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
