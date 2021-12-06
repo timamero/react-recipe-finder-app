053225f7dfbbbeba1531b7ec3f8c233bf041ad53
Fatima Camero <fcamero12@gmail.com>
2021-12-06 09:33:34 -0800

    remove duplicate header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Typeography from '@mui/material/Typography'
 import Home from './pages/Home'
 import LogIn from './pages/LogIn';
 import Register from './pages/Register'
@@ -9,7 +8,6 @@ import './firebasedb';
 function App() {
   return (
     <div className="App">
-      <Typeography variant="h1">Cravings</Typeography>
       <Home />
       <LogIn />
       <Register />
