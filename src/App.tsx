54a5aacf1ffb54049b95a9237e33c6ed983e673a
Fatima Camero <fcamero12@gmail.com>
2021-12-06 11:01:18 -0800

    import NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,17 @@
  * https://reactrouterdotcom.fly.dev/docs/en/v6/getting-started/installation#basic-installation
 */
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import LogIn from './pages/LogIn';
 import Register from './pages/Register'
 import './firebasedb';
-
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </nav>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
