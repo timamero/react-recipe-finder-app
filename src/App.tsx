121384499874c7a242b1da3939f779632b407663
Fatima Camero <fcamero12@gmail.com>
2021-12-08 18:08:34 -0800

    create AuthContext provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,41 @@
  * Resources:
  * https://reactrouterdotcom.fly.dev/docs/en/v6/getting-started/installation#basic-installation
 */
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import LogIn from './pages/LogIn';
 import Register from './pages/Register'
-import './firebasedb';
+import { auth } from './firebasedb';
 import NavBar from './components/NavBar';
+import { onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = createContext<string | null>('')
 
 function App() {
+  const [currentUser, setCurrentUser] = useState<string | null>('');
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user.email)
+      } else {
+        console.log('user is not logged in')
+      }
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
+    <AuthContext.Provider value={currentUser}>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
