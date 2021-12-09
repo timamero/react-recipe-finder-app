012ab3bafe518a0d4ae00d0abc0513dc43598959
Fatima Camero <fcamero12@gmail.com>
2021-12-08 18:12:11 -0800

    conditionally render elements based on if user is logged in

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,21 @@
  * Home page
  * 
 */ 
-
+import React, { useContext } from 'react';
 import { Container, Typography } from "@mui/material";
+import { AuthContext } from '../App';
 
 export default function Home() {
+  const user = useContext(AuthContext);
+
   return (
     <Container max-width="sm">
-      <Typography variant="h1">Cravings Recipe Finder</Typography>
+      {user
+        ?
+        <Typography variant="h1">Search for a recipe!</Typography>
+        :
+        <Typography variant="h1">Create an account to get started!</Typography>
+      }
     </Container>
   )
 }
