6b31b31696e7051245ef49bce591017c50c3969e
Fatima Camero <fcamero12@gmail.com>
2021-12-15 16:02:55 -0800

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,13 @@
  * Home page
  * 
 */ 
-import React, { useContext } from 'react';
+import React from 'react';
 import { Container, Typography } from "@mui/material";
-import { AuthContext } from '../App';
 
 export default function Home() {
-  const user = useContext(AuthContext);
-
   return (
     <Container max-width="sm">
-      {user
-        ?
-        <Typography variant="h1">Search for a recipe!</Typography>
-        :
-        <Typography variant="h1">Create an account to get started!</Typography>
-      }
+      <Typography variant="h1">Create an account to get started!</Typography>
     </Container>
   )
 }
