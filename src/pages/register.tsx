ce361b2f115c600dad353412005707e4a5cb467a
Fatima Camero <fcamero12@gmail.com>
2021-12-16 15:37:20 -0800

    Add setPersistence to authorization

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,11 +4,12 @@
  * Resources:
  * https://github.com/firebase/firebaseui-web
  * https://firebase.google.com/docs/auth/web/password-auth#before_you_begin
+ * https://firebase.google.com/docs/auth/web/auth-state-persistence
  * https://firebase.google.com/docs/emulator-suite
 */ 
 import React, { useState } from 'react'
 import { Container, Typography, Input, InputLabel, Button } from "@mui/material";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { auth } from '../firebasedb';
 import { useNavigate } from 'react-router-dom';
 
@@ -20,20 +21,21 @@ export default function Register() {
   const handleRegisterSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log('user', user)
-      })
-      .catch((error) => {
-        console.log('error in creating account')
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        if (process.env.NODE_ENV !== 'test') {
-          console.log('error code: ', errorCode);
-          console.log('error message: ', errorMessage)
-        }  
+    setPersistence(auth, browserLocalPersistence)
+    .then(() => createUserWithEmailAndPassword(auth, email, password))
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log('user', user)
+    })
+    .catch((error) => {
+      console.log('error in creating account')
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (process.env.NODE_ENV !== 'test') {
+        console.log('error code: ', errorCode);
+        console.log('error message: ', errorMessage)
+      }  
     });
 
     setEmail('')
